07ac4738cfde70fff9a175d77f520a74ef2fe11f
higorjsc <hh1217higorcampos@hotmail.com>
2025-04-03 15:52:44 -0300

    deploy

---
+++ assets/web-aWQnPTPu.js
@@ -0,0 +1,1 @@
+import{W as a}from"./PacksPage-13V-4OAX.js";import"./index-FvCZzgjW.js";class i extends a{async show(t){if(typeof document<"u"){let o=2e3;t.duration&&(o=t.duration==="long"?3500:2e3);const e=document.createElement("pwa-toast");e.duration=o,e.message=t.text,document.body.appendChild(e)}}}export{i as ToastWeb};
